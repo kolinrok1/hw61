73056e31bb9b3ebc5c0aef7a9bee1a004c3e69d6
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2023-05-02 22:12:42 +0300

    HW6

--- hw6-1/src/Main.java
+++ hw6-1/src/Main.java
@@ -1,5 +1,19 @@
+import FamilyTrees.FamilyTree;
+import Humans.Human;
+import Services.Service;
+import Sex.Sex;
+import pesenter.Presenter;
+import ui.Console;
+import ui.View;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        View view = new Console();
+        Service service = new Service();
+        Presenter presenter = new Presenter(view, service);
+        view.start();
     }
 }
